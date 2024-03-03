5734287e5128a7f9ccbad46915b6f294b2449aeb
Muhammad Ilyas <124496272+eDeveloper132@users.noreply.github.com>
2024-03-04 04:08:57 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import chalk from "chalk";
+import dotenv from "dotenv";
+import express from "express";
+import path from "path";
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 3400;
+app.use(express.json());
+app.use("*", (req, res, next) => {
+    next();
+});
+const currentDirectory = process.cwd();
+app.listen(PORT, () => console.log(chalk.green(`Server is running on port ${PORT}`)));
+// Default route
+app.get("/", (req, res) => {
+    res.sendFile(path.join(currentDirectory, "index.html"));
+});
+// Middleware for handling errors
+app.use("*", (req, res, next) => {
+    res.statusMessage = "Error";
+    next();
+});
+// Middleware to handle 404 errors
+app.use("*", (req, res) => {
+    res.status(404).sendFile(path.join(currentDirectory, "Error.html"));
+});
